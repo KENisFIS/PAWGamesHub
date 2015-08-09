9892961175ea1ba18ab4b9f64dbb80152dae6392
KENisFIS <KENisFIS@gmail.com>
2015-08-08 22:15:53 -0400

    merge test01

--- PAWGamesHub/src/us/pawgames/hub/PAWHub.java
+++ PAWGamesHub/src/us/pawgames/hub/PAWHub.java
@@ -1,19 +1,13 @@
 package us.pawgames.hub;
 
-import java.util.ArrayList;
-
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import us.pawgames.hub.events.HubEventHandler;
-import us.pawgames.hub.inventory.CustomItem;
 import us.pawgames.hub.perks.Pet;
 
 public class PAWHub extends JavaPlugin {
@@ -38,26 +32,6 @@ public class PAWHub extends JavaPlugin {
 		super.onDisable();
 	}
 
-	public void setPlayerInventory(Player player) {
-		Inventory playerInventory = player.getInventory();
-		player.getInventory().clear();
-		
-		CustomItem compass = new CustomItem(Material.COMPASS, 1);
-		compass.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Select a Game");
-		ArrayList<String> compassLore = new ArrayList<String>();
-		compassLore.add(ChatColor.AQUA + "Plan to have all available servers");
-		compassLore.add(ChatColor.AQUA + "listed with online status");
-		compass.setLore(compassLore);
-		playerInventory.setItem(0, compass);
-		
-		CustomItem pvpSword = new CustomItem(Material.DIAMOND_SWORD, 1);
-		pvpSword.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "PAW");
-		ArrayList<String> pvpSwordLore = new ArrayList<String>();
-		pvpSwordLore.add(ChatColor.AQUA + "You can use /sword [swordname]");
-		pvpSwordLore.add(ChatColor.AQUA + "to create your own custom sword.");
-		pvpSword.setLore(pvpSwordLore);
-		playerInventory.setItem(1, pvpSword);
-	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
--- PAWGamesHub/src/us/pawgames/hub/inventory/PlayerInventory.java
+++ PAWGamesHub/src/us/pawgames/hub/inventory/PlayerInventory.java
@@ -1,6 +1,11 @@
 package us.pawgames.hub.inventory;
 
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class PlayerInventory {
 	private Player player;
@@ -11,5 +16,27 @@ public class PlayerInventory {
 	
 	public Player getPlayer() {
 		return this.player;
+	}
+	
+	
+	public void setPlayerInventory() {
+		Inventory playerInventory = player.getInventory();
+		player.getInventory().clear();
+		
+		CustomItem compass = new CustomItem(Material.COMPASS, 1);
+		compass.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Select a Game");
+		ArrayList<String> compassLore = new ArrayList<String>();
+		compassLore.add(ChatColor.AQUA + "Plan to have all available servers");
+		compassLore.add(ChatColor.AQUA + "listed with online status");
+		compass.setLore(compassLore);
+		playerInventory.setItem(0, compass);
+		
+		CustomItem pvpSword = new CustomItem(Material.DIAMOND_SWORD, 1);
+		pvpSword.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "PAW");
+		ArrayList<String> pvpSwordLore = new ArrayList<String>();
+		pvpSwordLore.add(ChatColor.AQUA + "You can use /sword [swordname]");
+		pvpSwordLore.add(ChatColor.AQUA + "to create your own custom sword.");
+		pvpSword.setLore(pvpSwordLore);
+		playerInventory.setItem(1, pvpSword);
 	}
 }
