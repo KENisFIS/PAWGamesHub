bba41f7d1996ab238aea2932edced2b79ad28ffe
KENisFIS <KENisFIS@gmail.com>
2015-08-08 20:10:49 -0400

    PAWHub pet command

--- PAWGamesHub/src/us/pawgames/hub/PAWHub.java
+++ PAWGamesHub/src/us/pawgames/hub/PAWHub.java
@@ -8,6 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -27,52 +28,61 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import us.pawgames.hub.inventory.CustomItem;
+import us.pawgames.hub.perks.Pet;
+
 public class PAWHub extends JavaPlugin implements Listener {
-	
+
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
 		super.onEnable();
-		
+
 		getServer().getPluginManager().registerEvents(this, this);
-		
-		for(World world: getServer().getWorlds()) {
+
+		for (World world : getServer().getWorlds()) {
 			world.setWeatherDuration(0);
 			world.setStorm(false);
 		}
-		
+
 	}
-	
+
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
 		super.onDisable();
 	}
-	
+
 	public void setPlayerInventory(Player player) {
 		Inventory playerInventory = player.getInventory();
 		player.getInventory().clear();
 		
+		CustomItem compass = new CustomItem(Material.COMPASS, 1);
+		compass.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Select a Game");
 		ArrayList<String> compassLore = new ArrayList<String>();
-			compassLore.add(ChatColor.AQUA + "Plan to have all available servers");
-			compassLore.add(ChatColor.AQUA + "listed with online status");
-		PAWInventoryUtility.createDisplay(Material.COMPASS, playerInventory, 0, "Select a Server", compassLore);
-			
+		compassLore.add(ChatColor.AQUA + "Plan to have all available servers");
+		compassLore.add(ChatColor.AQUA + "listed with online status");
+		compass.setLore(compassLore);
+		playerInventory.setItem(0, compass);
+		
+		CustomItem pvpSword = new CustomItem(Material.DIAMOND_SWORD, 1);
+		pvpSword.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "PAW");
 		ArrayList<String> pvpSwordLore = new ArrayList<String>();
-			pvpSwordLore.add(ChatColor.DARK_RED + "PVP Sword is still in developement");
-			pvpSwordLore.add(ChatColor.DARK_RED + "and is still not working");
-			pvpSwordLore.add(ChatColor.RED + "Equip this sword to enable PvP");
-			pvpSwordLore.add(ChatColor.RED + "Unequip this sword to disable PvP");
-		PAWInventoryUtility.createDisplay(Material.DIAMOND_SWORD, playerInventory, 1, "PVP Toggle", pvpSwordLore);
+		pvpSwordLore.add(ChatColor.AQUA + "You can use /sword [swordname]");
+		pvpSwordLore.add(ChatColor.AQUA + "to create your own custom sword.");
+		pvpSword.setLore(pvpSwordLore);
+		playerInventory.setItem(1, pvpSword);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		sender.sendMessage("onCommand");
-		if(label.equalsIgnoreCase("pawtest") || command.getName().equalsIgnoreCase("pawtest") && sender.hasPermission(command.getPermission())) {
-			sender.sendMessage("passed if statement");
-			
-			
+		if(sender instanceof Player) {
+			Player player = (Player) sender;
+			sender.sendMessage("onCommand");
+			if (label.equalsIgnoreCase("pet")) {
+				sender.sendMessage("passed if statement");
+				new Pet(player, EntityType.COW);
+			}
 		}
 		return true;
 	}
