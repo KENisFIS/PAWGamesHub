6e837a55b063442a6ebeea19bbced120be91f699
KENisFIS <kenisfis@gmail.com>
2015-08-09 23:35:30 -0400

    Commit
    Change-Id: Ifa0bcc5a6cdd4ba04b0130f558b8105a90e90c1c
    Signed-off-by: KENisFIS <kenisfis@gmail.com>

--- PAWGamesHub/src/us/pawgames/hub/events/HubEventHandler.java
+++ PAWGamesHub/src/us/pawgames/hub/events/HubEventHandler.java
@@ -1,8 +1,6 @@
 package us.pawgames.hub.events;
 
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,18 +10,13 @@ import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 
-import us.pawgames.hub.PAWHub;
+import us.pawgames.hub.effects.Particle;
 import us.pawgames.hub.inventory.PlayerHubInventory;
 import us.pawgames.hub.pvp.EquipPvPGear;
 
 public class HubEventHandler implements Listener{
-	public HubEventHandler(PAWHub plugin) { 
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
 	private Plugin plugin;
 	
 	public HubEventHandler(Plugin plugin) {
@@ -34,25 +27,28 @@ public class HubEventHandler implements Listener{
 	private void inventoryClick(InventoryClickEvent event) {
 		if(event.getWhoClicked() instanceof Player) {
 			Player player = (Player) event.getWhoClicked();
-			if(event.getClickedInventory() == player.getInventory()) {
-				event.setCancelled(true);
+			if(player.getGameMode() != GameMode.CREATIVE) {
+				if(event.getClickedInventory() == player.getInventory()) {
+					event.setCancelled(true);
+				}
 			}
 		}
 	}
 	
 	@EventHandler
 	private void playerDropItem(PlayerDropItemEvent event) {
-		event.setCancelled(true);
+		if(event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+			event.setCancelled(true);
+		}
 	}
 	
 	@EventHandler
 	private void playerItemHeld(PlayerItemHeldEvent event) {
 		Player player = event.getPlayer();
 		EquipPvPGear equipment = new EquipPvPGear(player);
-		PlayerInventory inventory = player.getInventory();
-		if(inventory.getItem(event.getNewSlot()).getType() == Material.DIAMOND_SWORD) {
+		if(event.getNewSlot() == 1) {
 			equipment.diamondGear();
-		} else if(inventory.getItemInHand().getType() != Material.DIAMOND_SWORD || inventory.getItemInHand().hasItemMeta()){
+		} else if(event.getNewSlot() != 1){
 			equipment.unequip();
 		}
 	}
@@ -61,16 +57,18 @@ public class HubEventHandler implements Listener{
 	private void playerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		player.setGameMode(GameMode.ADVENTURE);
-		String joinMessage = ChatColor.LIGHT_PURPLE + "Welcome "
-							+ player.getDisplayName() + ChatColor.LIGHT_PURPLE
-							+ " to PAWGames";
-		player.sendMessage(joinMessage);
 		new PlayerHubInventory(player).setPlayerInventory();
+		if(player.hasPermission("particle.special")) {
+			new Particle(plugin).createPart(player);
+		}
+
 	}
 	
 	@EventHandler
 	private void playerPickupItem(PlayerPickupItemEvent event) {
-		event.setCancelled(true);
+		if(event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+			event.setCancelled(true);
+		}
 	}
 	
 	@EventHandler
