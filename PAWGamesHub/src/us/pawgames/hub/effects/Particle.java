a9e38dfd862d08c20da58811100ea1588cfeee2c
KENisFIS <KENisFIS@gmail.com>
2015-08-15 23:50:41 -0400

    today is the day I commit...

--- PAWGamesHub/src/us/pawgames/hub/effects/Particle.java
+++ PAWGamesHub/src/us/pawgames/hub/effects/Particle.java
@@ -1,49 +1,91 @@
 package us.pawgames.hub.effects;
 
-import java.util.Random;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
  
 public class Particle {
 	private Plugin plugin;
+	private ParticleEffect effect;
+	private Location location;
 	
-	public Particle(Plugin plugin) {
+	public Particle(Plugin plugin, ParticleEffect effect, Location center) {
 		this.plugin = plugin;
+		this.effect = effect;
+		this.location = center;
 	}
-	@EventHandler
-    	public void createPart(final Player player) {
-            new BukkitRunnable(){
-                double t = Math.PI/4;
-                Location loc = player.getLocation();
-                Random r = new Random();
-                public void run(){
-                    t = t + 0.1*Math.PI;
-                    for (double theta = 0; theta <= 2*Math.PI; theta = theta + Math.PI/32){
-                        double x = t*Math.cos(theta);
-                        double y = 2*Math.exp(-1.0*t) * Math.sin(t) + 1.5;
-                        double z = t*Math.sin(theta);
-                        loc.add(x,y,z);
-                        ParticleEffect.SPELL_WITCH.display(0, 0, 0, 0, 1, loc, 32);
-                        loc.subtract(x,y,z);
-                       
-                        theta = theta + Math.PI/64;
-                       
-                        x = t*Math.cos(theta);
-                        y = 2*Math.exp(-0.1*t) * Math.sin(t) + 1.5;
-                        z = t*Math.sin(theta);
-                        loc.add(x,y,z);
-                        ParticleEffect.CRIT_MAGIC.display(0, 0, 0, 0, 1, loc, 32);
-                        loc.subtract(x,y,z);
-                        }
-                        if (t > 4.8){
-                                this.cancel();
-                        }
-                }
-                        
-            }.runTaskTimer(plugin, 0, 1);
+
+	public void parametric(int delay, int duration) {
+		new BukkitRunnable(){
+			double t = Math.PI/4;
+			public void run(){
+            	t = t + 0.1*Math.PI;
+            	for (double theta = 0; theta <= 2*Math.PI; theta = theta + Math.PI/32){
+            		double x = t*Math.cos(theta);
+            		double y = 2*Math.exp(-1.0*t) * Math.sin(t) + 1.5;
+            		double z = t*Math.sin(theta);
+            		location.add(x,y,z);
+            		effect.display(0, 0, 0, 0, 1, location, 32);
+            		location.subtract(x,y,z);
+            		
+            		theta = theta + Math.PI/64;
+            		
+            		x = t*Math.cos(theta);
+            		y = 2*Math.exp(-0.1*t) * Math.sin(t) + 1.5;
+            		z = t*Math.sin(theta);
+            		location.add(x,y,z);
+            		effect.display(0, 0, 0, 0, 1, location, 32);
+            		location.subtract(x,y,z);
+            	}
+            	if (t > 4.8){
+            		this.cancel();
+            	}
+            }
+		}.runTaskTimer(plugin, delay, duration);
+	}
+	
+	public void xHeart(final Player player, final ParticleEffect effect, int delay, int period) {
+		//formula: (2x^2 + y^2 + z^2 - 1)^3 - x^2 * z^3 / 10 - y^2 * z^3 = 0
+		//             -3 <= x,y,z <= 3
+		
+		new BukkitRunnable() {
+			Location location = player.getLocation();
+			
+			@Override
+			public void run() {
+				
+			}
+		}.runTaskTimer(plugin, delay, period);
+	}
+	
+	public void zHeart(final Player player, final ParticleEffect effect, int ticks) {
+		//formula: (x^2 + (9 / 4) * y^2 + z^2 - 1)^3 - x^2 * z^3 - (9 / 80) * y^2 * z^3 = 0
+		//             -3 <= x,y,z <= 3
+	}
+	
+	public void sphere(final Player player,double startSize, double finalSize, int delay, int duration) {
+		new BukkitRunnable(){
+			double t = Math.PI/4;
+			Location location = player.getLocation();
+			//Random random = new Random();
+			@Override
+			public void run(){
+            	t = t + 0.1*Math.PI;
+            	for (double theta = 0; theta <= 2*Math.PI; theta = theta + Math.PI/32){
+            		double x = t*Math.cos(theta);
+            		double y = 2*Math.exp(-1.0*t) * Math.sin(t) + 1.5;
+            		double z = t*Math.sin(theta);
+            		location.add(x,y,z);
+            		effect.display(0, 0, 0, 0, 1, location, 32);
+            		location.subtract(x,y,z);
+            	}
+            	if (t > 4.8){
+            		this.cancel();
+            	}
+            }
+		}.runTaskTimer(plugin, 0, 1);
+	
+	}
 }
-}
+
--- PAWGamesHub/src/us/pawgames/hub/events/HubEventHandler.java
+++ PAWGamesHub/src/us/pawgames/hub/events/HubEventHandler.java
@@ -1,10 +1,14 @@
 package us.pawgames.hub.events;
 
+import java.util.Arrays;
+
+import org.apache.logging.log4j.core.config.plugins.PluginManager;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,7 +16,10 @@ import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.Plugin;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.event.ServerSwitchEvent;
 import us.pawgames.hub.effects.Particle;
+import us.pawgames.hub.effects.ParticleEffect;
 import us.pawgames.hub.inventory.PlayerHubInventory;
 import us.pawgames.hub.pvp.EquipPvPGear;
 
@@ -31,6 +38,17 @@ public class HubEventHandler implements Listener{
 				if(event.getClickedInventory() == player.getInventory()) {
 					event.setCancelled(true);
 				}
+			}
+		}
+	}
+	
+	@EventHandler
+	private void playerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+		Player sender = event.getPlayer();
+		String serverCommand = "/server ";
+		if(event.getMessage().startsWith(serverCommand)) {
+			for(Player onlinePlayer: sender.getWorld().getPlayers()) {
+				onlinePlayer.sendMessage(sender.getCustomName() + ChatColor.AQUA + " went to " + ChatColor.GREEN + event.getMessage().substring(7).toUpperCase());
 			}
 		}
 	}
@@ -59,9 +77,9 @@ public class HubEventHandler implements Listener{
 		player.setGameMode(GameMode.ADVENTURE);
 		new PlayerHubInventory(player).setPlayerInventory();
 		if(player.hasPermission("particle.special")) {
-			new Particle(plugin).createPart(player);
+			new Particle(plugin, ParticleEffect.SPELL_WITCH, player.getLocation()).parametric(0, 10);
+			new Particle(plugin, ParticleEffect.CRIT_MAGIC, player.getLocation()).parametric(10, 10);
 		}
-
 	}
 	
 	@EventHandler
